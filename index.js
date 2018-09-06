88cc377b855040fe2435d2ddd30fc7622dc103ac
Omar Azzat <oazzat19@gmail.com>
2018-09-06 18:39:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,16 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i=0; i<cart.length; i++){
+    if(item === cart[i].itemName){
+      delete cart[i]
+      return cart
+    }
+    else{
+      return "That item is not in your cart."
+    }
+
+  }
 }
 
 function placeOrder(cardNumber) {

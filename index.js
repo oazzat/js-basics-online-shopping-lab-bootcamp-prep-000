887ae25cbec169064f4bda697572c45bdcca3f99
Omar Azzat <oazzat19@gmail.com>
2018-09-06 18:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var obj = {itemName: item, itemPrice: 0}
+ obj.itemPrice = Math.floor(Math.random()*100)
+ cart.push(obj)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if(cart.length<1){
+    return "Your shopping cart is empty."
+  }
+  var sentence = `In your cart, you have `
+  
+  for(var i=0;i<cart.length; i++){
+    if((i+1) === cart.length){
+      sentence = sentence + " and " + cart[i].itemName + " at $" + cart[i].itemPrice + "."
+    }
+    else{
+    
+    sentence = sentence + cart[i].itemName + " at $" + cart[i].itemPrice + ", " 
+    }
+  }
+  return sentence
 }
 
 function total() {

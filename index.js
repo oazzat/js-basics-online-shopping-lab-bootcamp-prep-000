c562d83f1c1a3aaca1ebf6fadd869c3f7629a832
Omar Azzat <oazzat19@gmail.com>
2018-09-06 19:09:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,10 +64,10 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   
-  var total = cart.total()
+  var all = total()
   for(var i=0; i<cart.length;i++){
     cart.pop()
   }
   
-  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  return `Your total cost is $${all}, which will be charged to the card ${cardNumber}.`
 }

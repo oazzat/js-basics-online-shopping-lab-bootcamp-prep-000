c4ced4e717c7d1645d35cc5242e73c02c4daf876
Omar Azzat <oazzat19@gmail.com>
2018-09-06 18:45:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   for (var i=0; i<cart.length; i++){
     if(item === cart[i].itemName){
     var array = cart.splice(i,1)
-      return cart
+      return array
     }
     else{
       return "That item is not in your cart."
